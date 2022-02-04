ce9d4bb324d4c5e253eb573bf17afa4cff92150c
maddietimm <95003326+maddietimm@users.noreply.github.com>
2022-02-04 14:55:28 -0800

    Add files via upload

---
+++ hw_day_1/hw.js
@@ -0,0 +1,41 @@
+//==================Exercise #1 ==========//
+/*Write a function that takes in the string and the list of dog names, loops through 
+the list and checks that the current name is in the string passed in. The output should be:
+"Matched dog_name" if name is in the string, if no matches are present console.log "No Matches"
+*/
+let dog_string = "Hello Max, my name is Dog, and I have purple eyes!"
+let dog_names = ["Max", "HAS", "PuRple", "dog"]
+
+let findWords = (dog_string, dog_names) => {
+    for (let i = 0; i < dog_names.length; i++) {
+        if (dog_string.includes(dog_string[i])) {
+            return "Matched dog_name"
+        }
+    }
+    return "No Matches"
+};
+
+console.log(findWords(dog_string, dog_names))
+
+//============Exercise #2 ============//
+/*Write a fucntion that takes in an array and removes every value that's double is over 50.
+*/
+
+// given_arr == [13, 22, 26, 40, 1, 10]
+
+let replaceEvens = (arr) => {
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i] * 2 > 50) {
+            arr.splice(i, 1)
+            i--
+        }
+    }
+    return arr
+};
+
+console.log(replaceEvens([13, 22, 26, 40, 1, 10]))
+
+//Expected output
+//Given arr == [13, 22, 26, 40, 1, 10]
+//Output arr == [13, 22, 1, 10]
+//Explanation: 13*2 < 50, 22*2 < 50, 26*2 > 50, etc...
